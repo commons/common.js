74768a6fae94b1ec9eea8b0058d9211daaefb690
Makis Tracend <makis.tracend@gmail.com>
2013-03-06 17:56:47 -0800

    #3 new method to dynamically add scripts

--- build/common.js
+++ build/common.js
@@ -1,4 +1,5 @@
-
+// global namespace
+var c = {};
 
 // #1 - Universal console log
 // Source: https://github.com/h5bp/html5-boilerplate/blob/master/js/plugins.js
@@ -26,3 +27,37 @@
 }());
 
 
+// Load scripts on the fly
+// Usage: 
+//     c.script("//connect.facebook.net/en_US/all.js");
+//     c.script('http://platform.twitter.com/widgets.js', { id : 'twitter-wjs',  async : true});
+
+(function(d) {
+	
+	c.script = function( url, attr ){
+		
+		//fallbacks
+		attr = attr || {};
+		url = url || false;
+		attr.id = attr.id || false;
+		attr.async = attr.async || false;
+		// prerequisites
+		if( !url ) return;
+		if( attr.id && d.getElementById(attr.id) ) return;
+		// variables
+		var t = "script";
+		var js = d.createElement(t);
+		// clean url from protocol definition
+		url = url.replace(/^http:|^https:/, "");
+		// set attributes
+		js.type = 'text/javascript'; 
+		if( attr.id ) js.id = attr.id; 
+		js.async = attr.async;
+	 	js.src = ("https:"==location.protocol?"https:":"http:")+ url;
+		// place in DOM
+		var s = d.getElementsByTagName(t)[0]; 
+		s.parentNode.insertBefore(js, s);
+	
+	};
+	
+})(document);
