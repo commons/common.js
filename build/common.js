5382f80b0b819fe123dd49f4bb444c6b444e325c
Makis Tracend <makis.tracend@gmail.com>
2013-03-06 18:11:19 -0800

    adding new method to parse query strings

--- build/common.js
+++ build/common.js
@@ -61,3 +61,16 @@ var c = {};
 	};
 	
 })(document);
+
+
+// Parse query params
+c.query = function( url ) {
+	// fallback to the window location
+	url = url || window.location.href;
+    var vars = {};
+    var parts = url.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+        vars[key] = value;
+    });
+    return vars;
+};
+
